f40141362604ecd46077131c402157f32ff60f72
connectasp <connect.asp90@gmail.com>
2024-07-19 14:49:40 +0530

    update

--- 1-Python Basics/test.py
+++ 1-Python Basics/test.py
@@ -4,9 +4,12 @@ this is a exmaple of multiline comments
 '''
 print("Hello Github")
 print("Data Science")
-
-
-
+print("Hello Github")
+print("Data Science")
+print("Hello Github")
+print("Data Science")
+print("Hello Github")
+print("Data Science")
 
 
 
